273fb6b3f15c65d148bb923d8533f20c61ebe3e1
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-25 15:09:30 -0300

    feat: req 1 - add study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,22 @@
 def study_schedule(permanence_period, target_time):
-    raise NotImplementedError
+    # deacordo com course usando linear search
+    if type(target_time) != int:
+        return None
+
+    students_at_same_time = 0
+
+    n = len(permanence_period)
+    for index in range(0, n):
+        if (
+            type(permanence_period[index][0]) != int
+            or type(permanence_period[index][1]) != int
+        ):
+            return None
+
+        if (
+            permanence_period[index][0]
+            <= target_time
+            <= permanence_period[index][1]
+        ):
+            students_at_same_time += 1
+    return students_at_same_time
