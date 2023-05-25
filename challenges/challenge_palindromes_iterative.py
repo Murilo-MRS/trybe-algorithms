0bfe6a9d3b7dca20a75068b759d3c7decbd43081
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-25 17:42:07 -0300

    fix: req 6 - add is_palindrome(iterative)

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -3,7 +3,7 @@ def is_palindrome_iterative(word):
     # https://www.geeksforgeeks.org/python-program-check-string-palindrome-not/
     if len(word) == 0:
         return False
-    for index in range(0, int(len(str) / 2)):
-        if str[index] != str[len(str) - index - 1]:
+    for index in range(0, int(len(word) / 2)):
+        if word[index] != word[len(word) - index - 1]:
             return False
     return True
