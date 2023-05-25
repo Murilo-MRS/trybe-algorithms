102f1aec89c83cbeb5b60cc20ae69f42b3eda104
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-25 15:48:55 -0300

    test: req 2 - test encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("message", "key")
+
+    assert encrypt_message("message", 1) == "m_egasse"
+    assert encrypt_message("message", 2) == "egass_em"
+    assert encrypt_message("message", 3) == "sem_egas"
+    assert encrypt_message("message", 4) == "ega_ssem"
+
+    assert encrypt_message("message", 8) == "egassem"
+    assert encrypt_message("message", -1) == "egassem"
