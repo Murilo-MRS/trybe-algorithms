0d8585fc2fba2f3f1e5078783858b30f974ee884
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-25 17:40:30 -0300

    feat: req 6 - add is_palindrome(iterative)

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,9 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # baseado em
+    # https://www.geeksforgeeks.org/python-program-check-string-palindrome-not/
+    if len(word) == 0:
+        return False
+    for index in range(0, int(len(str) / 2)):
+        if str[index] != str[len(str) - index - 1]:
+            return False
+    return True
