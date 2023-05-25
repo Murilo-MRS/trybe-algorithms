4a80d31be62778361e66bb43739e62f4cdc095fa
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-25 16:05:43 -0300

    feat: req 3 - add is_palindrome(recursividade)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:  # caso base
+        return False
+    elif low_index >= high_index:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
