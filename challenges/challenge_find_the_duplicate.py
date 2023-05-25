5a091fb99eca0bd7eb7429a09e5d90fd63775d3c
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-25 18:50:36 -0300

    fix: req 5 - add find duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,20 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    """
+    para pegar todos sao numeros em uma lista refereencia:
+    https://datascienceparichay.com/article/python-check-list-contains-only-numbers/
+    """
+    if not isinstance(nums, list) or not all(
+        [isinstance(item, int) for item in nums]
+    ):
+        return False
+
+    nums.sort()
+    # baseado no código apresentado no dia 4.1
+    # pelo professor Cristiano Benites e ajuda da isabela costa
+    for index, num_element in enumerate(nums[:-1]):
+        if num_element == nums[index + 1] and num_element >= 0:
+            return num_element
+    return False
+
+
+print(find_duplicate([-1, -1, 2, 3, 10]))
